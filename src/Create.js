bb79666e14495ed8fe4393d833b03f6a22a6b730
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 17:48:26 +0800

    lesson 27: Controlled form inputs

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 const Create = () => {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('mario');
+
 	return (
 		<div className="create">
 			<h2>Add a new blog</h2>
+			<form>
+				<label>Blog title:</label>
+				<input
+					type="text"
+					required
+					defaultValue={ title }
+					onChange={ e => setTitle(e.target.value) }
+				/>
+				<label>Blog body:</label>
+				<textarea
+					required
+					defaultValue={ body }
+					onChange={ e => setBody(e.target.value) }
+				></textarea>
+				<label>Blog author:</label>
+				<select
+					defaultValue={ author }
+					onChange={ e => setAuthor(e.target.value) }
+				>
+					<option value="mario">Mario</option>
+					<option value="yoshi">Yoshi</option>
+				</select>
+				<button>Add Blog</button>
+				<p>{ title }</p>
+				<p>{ body }</p>
+				<p>{ author }</p>
+			</form>
 		</div>
 	);
 }
- 
+
 export default Create;
