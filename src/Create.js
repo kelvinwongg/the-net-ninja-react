90a8673c586da987cdc3ee211bd14d9922f6b675
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 18:21:26 +0800

    lesson 28: Submit form events

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,16 @@ const Create = () => {
 	const [title, setTitle] = useState('');
 	const [body, setBody] = useState('');
 	const [author, setAuthor] = useState('mario');
+	const handleSubmit = e => {
+		e.preventDefault();
+		const blog = { title, body, author };
+		console.log('form submitted:', blog);
+	}
 
 	return (
 		<div className="create">
 			<h2>Add a new blog</h2>
-			<form>
+			<form onSubmit={ handleSubmit }>
 				<label>Blog title:</label>
 				<input
 					type="text"
