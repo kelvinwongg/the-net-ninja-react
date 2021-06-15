a3ccdea6b2ea8ce4d6deb0619d752a7dae40adb6
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 11:19:37 +0800

    lesson 10

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-	// Non-reactive variable
-	// let name = 'mario';
-
-	// Reactive variable
-	const [name, setName] = useState('mario');
-	const [age, setAge] = useState(25);
-
-	const handleClick = () => {
-		setName('luigi');
-		setAge(30);
-	};
+	const [blogs, setBlogs] = useState([
+		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+		{ title: 'Webdev top tip', body: 'lorem ipsum...', author: 'luigi', id: 3 },
+	]);
 
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>{ name } is { age }</p>
-			<button onClick={ handleClick }>Click me</button>
+			{ blogs.map((blog) => (
+				<div className="blog-preview" key={ blog.id }>
+					<h2>{ blog.title }</h2>
+					<p>Written by: { blog.author }</p>
+				</div>
+			)) }
 		</div>
 	)
 }
