aca6c1ae320f1ad836635638072159e092db2426
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 18:40:44 +0800

    lesson 31: Delete request

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,18 @@
-import { div } from 'prelude-ls';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
 	const { id } = useParams();
 	const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id);
+	const history = useHistory();
+
+	const handleDelete = () => {
+		fetch('http://localhost:8000/blogs/' + blog.id, {
+			method: 'DELETE'
+		}).then(() => {
+			history.push('/');
+		})
+	}
 
 	return (
 		<div className="blog-details">
@@ -12,11 +20,12 @@ const BlogDetails = () => {
 			{ error && <div>{ error }</div> }
 			{ blog && (
 				<article>
-					<h2>{blog.title}</h2>
-					<p>Written by: {blog.author}</p>
-					<div>{blog.body}</div>
+					<h2>{ blog.title }</h2>
+					<p>Written by: { blog.author }</p>
+					<div>{ blog.body }</div>
+					<button onClick={ handleDelete }>delete</button>
 				</article>
-			)}
+			) }
 		</div>
 	);
 }
