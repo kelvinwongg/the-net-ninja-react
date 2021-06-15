ab0b7c5ba3bc81eeabb301bdb301265d4857919b
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 15:33:16 +0800

    lesson 19: API error

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
 	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
 
 	// Simulate API using json-server:
 	// npx json-server --watch data/db.json --port 8000 --delay 1000
 	useEffect(() => {
 		fetch('http://localhost:8000/blogs')
-			.then(res => res.json())
+			.then(res => {
+				if (!res.ok) {
+					throw Error('Could not fetch data from resource')
+				}
+				return res.json()
+			})
 			.then(data => {
-				console.log(data);
 				setBlogs(data);
+				setIsLoading(false);
+				setError(null);
+			})
+			.catch(err => {
+				setError(err.message);
 				setIsLoading(false);
 			})
 	}, []);
 
 	return (
 		<div className="home">
+			{ error && <div>{ error }</div> }
 			{ isLoading && <div>Loading...</div> }
 			{ blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
 		</div>
