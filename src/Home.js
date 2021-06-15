3d8df63a4ad7d260c6391de7435d466a0a786a5e
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 11:48:35 +0800

    lesson 14: useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,12 @@ const Home = () => {
 		const newBlogs = blogs.filter(blog => blog.id !== id)
 		setBlogs(newBlogs);
 	}
+
+	// Executed (after?) every render
+	useEffect(() => {
+		console.log('useEffect ran');
+		console.log(blogs);
+	});
 
 	return (
 		<div className="home">
