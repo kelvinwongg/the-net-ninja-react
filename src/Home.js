1bd10c25e5a9aa14c1f2710002d24626f300dbb2
Kelvin@MBP <kelvin.wyh@tti.com.hk>
2021-06-15 15:22:25 +0800

    lesson 18: Conditional template

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 
+	// Simulate API using json-server:
+	// npx json-server --watch data/db.json --port 8000 --delay 1000
 	useEffect(() => {
 		fetch('http://localhost:8000/blogs')
 			.then(res => res.json())
 			.then(data => {
 				console.log(data);
 				setBlogs(data);
+				setIsLoading(false);
 			})
 	}, []);
 
 	return (
 		<div className="home">
+			{ isLoading && <div>Loading...</div> }
 			{ blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
 		</div>
 	)
